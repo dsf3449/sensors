7afe4e418260c0aa810aca0a98fc43aa89333c66
Miles, Brian (CGI Federal) <brian.miles@cgifederal.com>
2017-04-25 15:21:35 -0500

    LEARN-83: setuptools packaging

--- sensors/sensors/common/constants.py
+++ sensors/sensors/common/constants.py
@@ -1,7 +1,7 @@
 import os
 
 LOGGER_NAME = "sensors"
-LOGGER_FILE = os.environ.get("LOGGER_PATH", "/val/log/sensor.log")
+LOGGER_FILE = os.environ.get("LOGGER_PATH", "/var/log/sensor.log")
 
 DB_PATH = os.environ.get("DB_PATH", "/var/spool/mqueue/sensor.sqlite")
 
